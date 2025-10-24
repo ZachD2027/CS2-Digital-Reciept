b7baf7f3f3f94927200fdd4b6a077f7d06b73ccd
Zach <zachary.dowling2027@gbwl.org>
2025-10-24 12:55:42 +0000

    Pat B

--- Main.java
+++ Main.java
@@ -1,6 +1,23 @@
-public class Main {
+  public class Main {
+  public static void main(String [] args) {
 
-   public static void main(String []args) {
-      System.out.println("Hello World");
-   }
-}
+   //CODE FOR PART B STARTS HERE
+   String highSchoolName = "Birch Wathen Lenox";
+   double priceDrink = 1.50;
+   double priceCandy = 1.25;
+   double priceHotDog = 2.75;
+   double priceHamburger = 3.50;
+   
+     // RECEIPT PRINTOUT SECTION
+     System.out.println("**************************************");
+     System.out.println("*                                    *");
+     System.out.println("*     "+ highSchoolName + " Snack Bar          *");
+     System.out.println("*                                    *");
+     System.out.println("*     Drink .......... $"+ priceDrink + "           *");                      
+     System.out.println("*     Candy .......... $"+ priceCandy + "          *");     
+     System.out.println("*     Hot Dog ........ $"+ priceHotDog + "          *");     
+     System.out.println("*     Hamburger ...... $"+ priceHamburger + "        *");     
+     System.out.println("*                                    *");    
+     System.out.println("**************************************");
+  }
+  }
