04cc20690b7204348f893324c8c510ea48ace389
Zach <zachary.dowling2027@gbwl.org>
2025-10-27 13:40:27 +0000

    Part C

--- Main.java
+++ Main.java
@@ -3,20 +3,30 @@
 
    //CODE FOR PART B STARTS HERE
    String highSchoolName = "Birch Wathen Lenox";
-   double priceDrink = 1.50;
+   double priceDrink = 2.99;
    double priceCandy = 1.25;
-   double priceHotDog = 2.75;
-   double priceHamburger = 3.50;
+   double priceHotDog = 4.75;
+   double priceHamburger = 6.25;
+
+   int orderNumber = (int) (Math.random()*100);
+   int numberDrink = (int) (Math.random()*100);
+   int numberCandy = (int) (Math.random()*100);
+   int numberHotDog = (int) (Math.random()*100);
+   int numberHamburger = (int) (Math.random()*100);
+   double taxRate = 0.08;
+   double subtotal = priceDrink*numberDrink+priceCandy*numberCandy+priceHotDog*numberHotDog+priceHamburger*numberHamburger;
+   double taxTotal = subtotal*taxRate;
+   double total = subtotal*taxTotal;
    
      // RECEIPT PRINTOUT SECTION
      System.out.println("**************************************");
      System.out.println("*                                    *");
-     System.out.println("*     "+ highSchoolName + " Snack Bar          *");
+     System.out.println("*    "+ highSchoolName + " Snack Bar    *");
      System.out.println("*                                    *");
-     System.out.println("*     Drink .......... $"+ priceDrink + "           *");                      
-     System.out.println("*     Candy .......... $"+ priceCandy + "          *");     
-     System.out.println("*     Hot Dog ........ $"+ priceHotDog + "          *");     
-     System.out.println("*     Hamburger ...... $"+ priceHamburger + "        *");     
+     System.out.println("*     Drink .......... $"+ priceDrink + "         *");                      
+     System.out.println("*     Candy .......... $"+ priceCandy + "         *");     
+     System.out.println("*     Hot Dog ........ $"+ priceHotDog + "         *");     
+     System.out.println("*     Hamburger ...... $"+ priceHamburger + "         *");     
      System.out.println("*                                    *");    
      System.out.println("**************************************");
   }
