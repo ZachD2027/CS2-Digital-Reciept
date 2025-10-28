464220bdb05c5d329a15ac514bba3a9fdce9d0c5
Zach <zachary.dowling2027@gbwl.org>
2025-10-28 14:58:23 +0000

    Done

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-  import java.util.Scanner;
-  public class Main {
+import java.util.Scanner;
+public class Main {
   public static void main(String [] args) {
   Scanner input = new Scanner(System.in);
   System.out.println("enter amount of drinks");
@@ -12,7 +12,11 @@
   int xhamburger = input.nextInt();
 
    //CODE FOR PART B STARTS HERE
-   String highSchoolName = "Birch Wathen Lenox";
+   input.nextLine();
+   System.out.println("enter high school name");
+   String highSchoolName = input.nextLine();
+   String initials = "BWL";
+
    double priceDrink = 2.99;
    double priceCandy = 1.25;
    double priceHotDog = 4.75;
@@ -27,12 +31,12 @@
    double taxRate = 0.08;
    double subtotal = priceDrink*numberDrink+priceCandy*numberCandy+priceHotDog*numberHotDog+priceHamburger*numberHamburger;
    double taxTotal = subtotal*taxRate;
-   double total = subtotal*taxTotal;
+   double total = subtotal+taxTotal;
    
      // RECEIPT PRINTOUT SECTION
      System.out.println("**************************************");
      System.out.println("*                                    *");
-     System.out.println("*    "+ highSchoolName + " Snack Bar    *");
+     System.out.println("*         "+ initials + " Snack Bar              *");
      System.out.println("*                                    *");
      System.out.println("*     Order #"+ orderNumber + "                       *");
      System.out.println("*     Drink .......... $"+ priceDrink + " #" + xdrink + "      *");                      
@@ -40,7 +44,7 @@
      System.out.println("*     Hot Dog ........ $"+ priceHotDog + " #" + xhotdog + "      *");     
      System.out.println("*     Hamburger ...... $"+ priceHamburger + " #" + xhamburger + "      *");     
      System.out.println("*                                    *"); 
-     System.out.println("*      Total ...... $"+ subtotal +"    *");   
+     System.out.println("*        Total ...... $"+ subtotal +"         *");
      System.out.println("**************************************");
   }
-  }
+}
